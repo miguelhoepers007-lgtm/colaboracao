afb461942dd00f27e8e651d25178fbd0e2ea628a
Miguel Hoepers <miguel.hoepers007@gmail.com>
2026-05-18 19:20:07 -0300

    teste emvio

---
+++ Idade.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Idade {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double idade = 12;
+        if (idade >= 0 && idade <= 12)
+            System.out.println("criança");
+        else if (idade >= 12 && idade <= 17) {
+            System.out.println("adolecente");
+        } else if (idade >= 18 && idade <= 59) {
+            System.out.println("adulto");
+        } else {
+            System.out.println("idoso");
+        }
+    }
+}
