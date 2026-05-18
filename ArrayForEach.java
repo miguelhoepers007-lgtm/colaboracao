6c7f0e907f956a73dc41d5f1c9abb48f18a7ebff
Miguel Hoepers <miguel.hoepers007@gmail.com>
2026-05-18 20:19:10 -0300

    Efetuando commit

---
+++ ArrayForEach.java
@@ -0,0 +1,10 @@
+public class ArrayForEach {
+    public static void main(String[] args) {
+
+        int[] numeros = {5, 10, 15, 20};
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+}
